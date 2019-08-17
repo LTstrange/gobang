26ab4b8d335d76112fa07b59ce5369e31309f963
LTstrange <835422774@qq.com>
2019-08-17 11:11:35 +0800

    初始化，定义模型。

---
+++ python_algorithm/models.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class ValueNet(nn.Module):
+    def __init__(self):
+        super(nn.Module, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class policyNet(nn.Module):
+    def __init__(self):
+        super(nn.Module, self).__init__()
+
+    def forward(self):
+        pass
+
+
